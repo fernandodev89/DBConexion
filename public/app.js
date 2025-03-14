d69e0eed5155cfc858871312f8e4a7c956a7a54c
Fernando Contreras <fernandomorann999@gmail.com>
2025-03-13 18:11:42 -0600

    restableciendo proyecto

--- public/app.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("btn").addEventListener("click", async () => {
-   const nombre = document.getElementById("nombre").value;
-   await fetch("/crear", {
-       method: "POST",
-       headers: { "Content-Type": "application/json" },
-       body: JSON.stringify({ nombre })
-   });
-   alert("Dato agregado");
-});
